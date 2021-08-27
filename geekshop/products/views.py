c250ba40d196a5a9fe00450e5c2f977a2ef3f14f
Nikolos123 <Isponec2>
2021-08-27 23:12:29 +0300

    lesson-2

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 #Контролер - функция
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'products/index.html')
 
 def products(request):
-    return render(request,'products.html')
+    return render(request, 'products/products.html')
+
+
+
+
+
+
 
