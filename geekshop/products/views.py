538b387230f3df77afbdc0833edb08159cbad583
Nikolos123 <Isponec2>
2021-09-02 20:23:11 +0300

    lesson3

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
 
+import os
+import json
+
+MODULE_DIR = os.path.dirname(__file__)
+
 # Create your views here.
-#Контролер - функция
+# Контролер - функция
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {'title': 'GeekShop'}
+    return render(request, 'products/index.html', context)
+
 
 def products(request):
-    return render(request, 'products/products.html')
-
-
-
-
-
-
-
+    context = {'title': 'Каталог'}
+    path_file = os.path.join(MODULE_DIR,'fixtures/goods.json')
+    context['products'] =json.load(open(path_file,encoding='utf-8'))
+    return render(request, 'products/products.html', context)
