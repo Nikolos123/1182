fd92124162e5d97975921859b322146df9891c9b
nik <isp06@mail.ru>
2021-09-16 21:19:51 +0300

    added paginator for products

--- geekshop/products/urls.py
+++ geekshop/products/urls.py
@@ -23,5 +23,6 @@ from products.views import products
 urlpatterns = [
     path('', products, name='index'),
     path('category/<int:id>', products, name='category'),
+    path('page/<int:page>', products, name='page'),
 ]
 
--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+
 import os
 import json
 
@@ -15,16 +17,19 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request,id=None):
-    # products_filter =
+def products(request,id=None,page=1):
 
-    # if id != None:
-    #     products_filter = Product.objects.filter(category_id = id)
-    # else:
-    #     products_filter = Product.objects.all()
+    products = Product.objects.filter(category_id = id) if id != None else Product.objects.all()
+    paginator = Paginator(products,per_page=3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
     context = {'title': 'Каталог',
                'category':  ProductsCategory.objects.all(),
                }
-    # context['products'] = products_filter
-    context.update({'products': Product.objects.filter(category_id = id)  if id != None  else Product.objects.all() })
+    context['products'] = products_paginator
     return render(request, 'products/products.html', context)
