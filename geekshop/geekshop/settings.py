fe6a752b71b06b26b3f49539196b8f95ab86f5d9
nik <isp06@mail.ru>
2021-09-23 23:10:14 +0300

    part_2_lesson_1

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -133,7 +133,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL='users.User'
 LOGIN_URL = '/users/login/'
-DOMAIN_NAME = 'http://localhost:8000'
+DOMAIN_NAME = 'http://localhost'
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = '25'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
@@ -141,8 +141,8 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
     # True if os.getenv('EMAIL_USE_SSL') == 'True' else False
 
-EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
-EMAIL_FILE_PATH = 'tmp/emails/'
+# EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# EMAIL_FILE_PATH = 'tmp/emails/'
 
-# EMAIL_HOST_USER ,EMAIL_HOST_PASSWORD = None,None
+EMAIL_HOST_USER ,EMAIL_HOST_PASSWORD = None,None
 # python -m smtpd -n -c DebuggingServer localhost:25
