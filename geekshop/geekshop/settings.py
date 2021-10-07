c42e69c5123363d9c89e357a9995df1b73bcd090
nik <isp06@mail.ru>
2021-10-07 21:14:03 +0300

    publication

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k4w8&#m9!cl29j6ri=rk_*3ui&+z7l!k4o@&n#-#x7ii-7j-#-'
 from dotenv import load_dotenv
 
-load_dotenv(BASE_DIR / '.env')
+# load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
