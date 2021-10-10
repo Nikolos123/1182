2ead13de661d9dbd21be8ae56d24ac5a982eb232
Nikolos123 <Isponec2>
2021-10-10 23:35:17 +0300

    test

--- geekshop/products/context_processors.py
+++ geekshop/products/context_processors.py
@@ -4,7 +4,7 @@ from baskets.models import Basket
 def basket(request):
     baskets_list = []
     if request.user.is_authenticated:
-        baskets_list = Basket.objects.filter(user=request.user)
+        baskets_list = Basket.objects.filter(user=request.user).select_related()
     return {
         'baskets':baskets_list
     }
