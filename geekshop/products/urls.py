b6dee14dddfd1c86ce4b48fb8e500fc75bf55991
Nikolos123 <Isponec2>
2021-10-14 21:07:07 +0300

    cached

--- geekshop/products/urls.py
+++ geekshop/products/urls.py
@@ -22,7 +22,8 @@ app_name = 'products'
 from products.views import products,ProductDetail
 
 urlpatterns = [
-    path('', cache_page(3600)(products), name='index'),
+    # path('', cache_page(3600)(products), name='index'),
+    path('', products, name='index'),
     path('category/<int:id>', products, name='category'),
     path('page/<int:page>', products, name='page'),
     path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,28 +1,59 @@
+from django.conf import settings
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from django.core.cache import cache
 
 import os
 import json
 
 from django.views.generic import DetailView
 
-from products.models import Product,ProductsCategory
+from products.models import Product, ProductsCategory
 
 MODULE_DIR = os.path.dirname(__file__)
 
+
 # Create your views here.
 # Контролер - функция
+
+def get_links_category():
+    if settings.LOW_CACHE:
+        key = 'links_category'
+        links_category = cache.get(key)
+
+        if links_category is None:
+            links_category = ProductsCategory.objects.filter(is_active=True)
+            cache.set(key, links_category)
+        return links_category
+    else:
+        return ProductsCategory.objects.filter(is_active=True)
+
+
+def get_links_product():
+    if settings.LOW_CACHE:
+        key = 'links_product'
+        links_product = cache.get(key)
+
+        if links_product is None:
+            links_product = Product.objects.filter(is_active=True).select_related()
+            cache.set(key, links_product)
+        return links_product
+    else:
+        return Product.objects.filter(is_active=True).select_related()
+
 
 def index(request):
     context = {'title': 'GeekShop'}
     return render(request, 'products/index.html', context)
 
 
-def products(request,id=None,page=1):
-
-    products = Product.objects.filter(category_id = id).select_related('category') if id != None else Product.objects.all().select_related('category')
-    paginator = Paginator(products,per_page=3)
+def products(request, id=None, page=1):
+    products = Product.objects.filter(category_id=id).select_related(
+        'category') if id != None else Product.objects.all().select_related('category')
+    products = get_links_product()
+    paginator = Paginator(products, per_page=3)
     try:
         products_paginator = paginator.page(page)
     except PageNotAnInteger:
@@ -31,11 +62,10 @@ def products(request,id=None,page=1):
         products_paginator = paginator.page(paginator.num_pages)
 
     context = {'title': 'Каталог',
-               'category':  ProductsCategory.objects.all(),
+               'category': get_links_category(),
                }
     context['products'] = products_paginator
     return render(request, 'products/products.html', context)
-
 
 
 class ProductDetail(DetailView):
