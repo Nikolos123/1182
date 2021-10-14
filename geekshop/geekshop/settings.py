b5d6460b7d775f587f07563fc225e842b57bcf9f
Nikolos123 <Isponec2>
2021-10-14 20:32:54 +0300

    cached

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -227,13 +227,13 @@ CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 120
 CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-
-}
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#         'LOCATION': '127.0.0.1:11211',
+#     }
+#
+# }
 
 LOW_CACHE = True
 
