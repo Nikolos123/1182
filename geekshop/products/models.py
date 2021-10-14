f6ae8777bb1af5641c35f86263d432f18eaf2421
Nagorniy <nagorniy@numatech.ru>
2021-10-14 14:19:53 +0300

    cached temolates

--- geekshop/products/models.py
+++ geekshop/products/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class ProductsCategory(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True, db_index=True)
 
     def __str__(self):
         return self.name
